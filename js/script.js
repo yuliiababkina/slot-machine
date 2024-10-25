8b1dd2d5e145868e2165687669ff1c49f3edb325
Yuliia Babkina <yulichka.babkina1312@gmail.com>
2024-10-25 18:01:30 +0300

    finished

---
+++ js/script.js
@@ -0,0 +1,41 @@
+const startBtn = document.querySelector(".start-btn");
+const container = document.querySelector(".container");
+const result = document.querySelector(".result");
+
+startBtn.addEventListener("click", handleStart);
+
+function handleStart() {
+    startBtn.disabled = true;
+    result.innerHTML = "";
+
+    const promises = [...container.children].map(() => {
+        return new Promise((resolve, reject) => {
+            const random = Math.random();
+
+            if (random > 0.5) {
+                resolve("🇧🇷");
+            } else {
+                reject("😈");
+            }
+        });
+    });
+
+    Promise.allSettled(promises).then((data) => {
+        data.forEach((item, i) => {
+            container.children[i].textContent = "";
+
+            setTimeout(() => {
+                container.children[i].textContent = item.value || item.reason;
+
+                if (i === data.length - 1) {
+                    result.innerHTML = isWinner ? "Vamos!" : "Tente novamente!";
+                    startBtn.disabled = false;
+                }
+            }, 1000 * (i + 1));
+        });
+
+        const isWinner = data.every((item) => {
+            item.status === "fulfilled";
+        });
+    });
+}
